2ec1f2df2383c0d84306c88d55e3b9d8a4d8900f
vladislavvv <vladislav713@yandex.ru>
2023-02-05 00:49:24 +0700

    init

---
+++ test_sources/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+  let a: u32 = 2;
+  let n: u32 = 10;
+  println!("Hello from Rust! Do you know that {}^{} = {}?", a, n, a.pow(n));
+}
---
+++ test_sources/main.ts
@@ -0,0 +1,5 @@
+function main(arg: string) {
+  console.log(arg)
+}
+
+main('Hello from TypeScript!')
